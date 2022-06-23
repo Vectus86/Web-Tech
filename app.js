16b0eae1b2e31a86b52d88b36cbc5fc22326d54d
calvinlawrence88 <calvinlawrence673@gmail.com>
2022-06-23 14:09:59 +0800

    load

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ menu.addEventListener('click', function () {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+var loader = document.getElementById('preloader');
+
+window.addEventListener('load', function () {
+  loader.style.display = 'none';
+});
